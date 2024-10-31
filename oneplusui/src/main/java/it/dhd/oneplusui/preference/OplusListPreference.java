21d1c04074ebf439296f2a585e3e9e87964034d8
DHD2280 <luigiconte98@gmail.com>
2024-10-31 23:33:43 +0100

    OplusListPreference: fix dividers

--- oneplusui/src/main/java/it/dhd/oneplusui/preference/OplusListPreference.java
+++ oneplusui/src/main/java/it/dhd/oneplusui/preference/OplusListPreference.java
@@ -3,6 +3,7 @@ package it.dhd.oneplusui.preference;
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
+import android.view.View;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -10,14 +11,18 @@ import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.preference.ListPreference;
 import androidx.preference.PreferenceViewHolder;
+import androidx.recyclerview.widget.OplusRecyclerView;
 
 import it.dhd.oneplusui.R;
 import it.dhd.oneplusui.appcompat.cardlist.CardListHelper;
+import it.dhd.oneplusui.appcompat.cardlist.CardListSelectedItemLayout;
 
-public class OplusListPreference extends ListPreference {
+public class OplusListPreference extends ListPreference implements OplusRecyclerView.IOplusDividerDecorationInterface{
 
     private final Context mContext;
     private String mForcePosition = null;
+    private View mItemView, mTitleView;
+    private final int mDividerDefaultHorizontalPadding;
 
     public OplusListPreference(@NonNull Context context) {
         this(context, null);
@@ -40,6 +45,7 @@ public class OplusListPreference extends ListPreference {
         if (a.hasValue(R.styleable.OplusListPreference_forcePosition)) {
             mForcePosition = a.getString(R.styleable.OplusListPreference_forcePosition);
         }
+        this.mDividerDefaultHorizontalPadding = context.getResources().getDimensionPixelSize(R.dimen.preference_divider_default_horizontal_padding);
         a.recycle();
         setLayoutResource(R.layout.oplus_preference);
     }
@@ -47,6 +53,8 @@ public class OplusListPreference extends ListPreference {
     @Override
     public void onBindViewHolder(@NonNull PreferenceViewHolder holder) {
         super.onBindViewHolder(holder);
+        mItemView = holder.itemView;
+        mTitleView = holder.findViewById(android.R.id.title);
         if (mForcePosition != null) {
             int pos = switch (mForcePosition) {
                 case "top" -> CardListHelper.HEAD;
@@ -63,6 +71,38 @@ public class OplusListPreference extends ListPreference {
         TextView summary = (TextView) holder.findViewById(android.R.id.summary);
         summary.setTextColor(ContextCompat.getColor(mContext, android.R.color.system_accent1_400));
 
+    }
+
+    @Override
+    public boolean drawDivider() {
+        if (!(this.mItemView instanceof CardListSelectedItemLayout)) {
+            return false;
+        }
+        if (mForcePosition != null) {
+            return mForcePosition.equals("middle") || mForcePosition.equals("top");
+        }
+        int positionInGroup = CardListHelper.getPositionInGroup(this);
+        return positionInGroup == 1 || positionInGroup == 2;
+    }
+
+    @Override
+    public View getDividerEndAlignView() {
+        return null;
+    }
+
+    @Override
+    public int getDividerEndInset() {
+        return this.mDividerDefaultHorizontalPadding;
+    }
+
+    @Override
+    public View getDividerStartAlignView() {
+        return this.mTitleView;
+    }
+
+    @Override
+    public int getDividerStartInset() {
+        return this.mDividerDefaultHorizontalPadding;
     }
 
 }
