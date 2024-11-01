0b89dcb1aad59f20458bdc38da378cbedb334c0a
DHD2280 <luigiconte98@gmail.com>
2024-11-01 13:25:14 +0100

    script changes

---
+++ .github/bumpVersion.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+NEWVERNAME=${GITHUB_REF_NAME}
+
+sed -i 's/version =.*/version = '$NEWVERNAME'/' oneplusui/build.gradle.kts
+sed -i 's/com.github.DHD2280:Oneplus-UI:.*/com.github.DHD2280:Oneplus-UI:'$NEWVERNAME'/' README.md
+
+# module changelog
+echo "**$NEWVERNAME**  " > newChangeLog.md
+cat changeLog.md >> newChangeLog.md
+echo "  " >> newChangeLog.md
+cat CHANGELOG.md >> newChangeLog.md
+mv  newChangeLog.md CHANGELOG.md
+
+# release message
+echo "**$NEWVERNAME**  " > release.msg
+echo "  " >> release.msg
+echo "*Changelog:*  " >> release.msg
+cat changeLog.md >> release.msg
+echo 'RMessage<<EOF' >> $GITHUB_ENV
+cat release.msg >> $GITHUB_ENV
+echo 'EOF' >> $GITHUB_ENV
---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+**1.1.1**
+- Fixed divider for OplusListPreference
+
+**1.1.0**
+- Added OplusRecyclerView
+- Added custom PreferenceFragment
+- Added dividers
+
+**1.0.0**
+- Initial release
