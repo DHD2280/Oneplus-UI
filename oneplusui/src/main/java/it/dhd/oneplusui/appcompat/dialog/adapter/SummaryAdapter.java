7e382be4f4df68e362fd457cf1e971fd20501a93
DHD2280 <luigiconte98@gmail.com>
2024-11-04 16:18:34 +0100

    CHANGELOG: Improved SummaryAdapter

--- oneplusui/src/main/java/it/dhd/oneplusui/appcompat/dialog/adapter/SummaryAdapter.java
+++ oneplusui/src/main/java/it/dhd/oneplusui/appcompat/dialog/adapter/SummaryAdapter.java
@@ -32,66 +32,64 @@ public class SummaryAdapter extends BaseAdapter {
         }
     }
 
-    public SummaryAdapter(Context context, boolean z, boolean z2, CharSequence[] charSequenceArr, CharSequence[] charSequenceArr2, int[] iArr) {
-        this.mIsTop = z;
-        this.mIsBottom = z2;
+    public SummaryAdapter(Context context, boolean isTop, boolean isBottom, CharSequence[] items, CharSequence[] descriptions, int[] textColors) {
+        this.mIsTop = isTop;
+        this.mIsBottom = isBottom;
         this.mContext = context;
-        this.mItems = charSequenceArr;
-        this.mSummaries = charSequenceArr2;
-        this.mTextColor = iArr;
+        this.mItems = items;
+        this.mSummaries = descriptions;
+        this.mTextColor = textColors;
     }
 
     private void resetPadding(int i, View view) {
-        int dimensionPixelSize2 = this.mContext.getResources().getDimensionPixelSize(R.dimen.oplus_bottom_alert_dialog_vertical_button_padding_vertical_new);
+        int paddingVertical = this.mContext.getResources().getDimensionPixelSize(R.dimen.oplus_bottom_alert_dialog_vertical_button_padding_vertical_new);
         int paddingLeft = view.getPaddingLeft();
         int paddingRight = view.getPaddingRight();
         if (i == getCount() - 1 && this.mIsBottom) {
-            view.setPadding(paddingLeft, dimensionPixelSize2, paddingRight, dimensionPixelSize2);
+            view.setPadding(paddingLeft, paddingVertical, paddingRight, paddingVertical);
         } else if (i == 0 && this.mIsTop) {
-            view.setPadding(paddingLeft, dimensionPixelSize2, paddingRight, dimensionPixelSize2);
+            view.setPadding(paddingLeft, paddingVertical, paddingRight, paddingVertical);
         } else {
-            view.setPadding(paddingLeft, dimensionPixelSize2, paddingRight, dimensionPixelSize2);
+            view.setPadding(paddingLeft, paddingVertical, paddingRight, paddingVertical);
         }
     }
 
-    @Override // android.widget.Adapter
+    @Override
     public int getCount() {
-        CharSequence[] charSequenceArr = this.mItems;
-        if (charSequenceArr == null) {
+        if (mItems == null) {
             return 0;
         }
-        return charSequenceArr.length;
+        return mItems.length;
     }
 
-    @Override // android.widget.Adapter
+    @Override
     public long getItemId(int i) {
         return i;
     }
 
-    public CharSequence getSummary(int i) {
-        CharSequence[] charSequenceArr = this.mSummaries;
-        if (charSequenceArr == null || i >= charSequenceArr.length) {
+    public CharSequence getSummary(int position) {
+        if (mSummaries == null || position >= mSummaries.length) {
             return null;
         }
-        return charSequenceArr[i];
+        return mSummaries[position];
     }
 
     @Override
-    public View getView(int i, View view, ViewGroup viewGroup) {
+    public View getView(int position, View view, ViewGroup viewGroup) {
         ViewHolder viewHolder;
         if (view == null) {
             view = LayoutInflater.from(this.mContext).inflate(LAYOUT, viewGroup, false);
             viewHolder = new ViewHolder();
-            viewHolder.mItemView = (TextView) view.findViewById(android.R.id.text1);
-            viewHolder.mSummaryView = (TextView) view.findViewById(R.id.summary_text2);
-            viewHolder.mDivider = (ImageView) view.findViewById(R.id.item_divider);
-            viewHolder.mMainLayout = (LinearLayout) view.findViewById(R.id.main_layout);
+            viewHolder.mItemView = view.findViewById(android.R.id.text1);
+            viewHolder.mSummaryView = view.findViewById(R.id.summary_text2);
+            viewHolder.mDivider = view.findViewById(R.id.item_divider);
+            viewHolder.mMainLayout = view.findViewById(R.id.main_layout);
             view.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) view.getTag();
         }
-        CharSequence item = getItem(i);
-        CharSequence summary = getSummary(i);
+        CharSequence item = getItem(position);
+        CharSequence summary = getSummary(position);
         viewHolder.mItemView.setText(item);
         if (TextUtils.isEmpty(summary)) {
             viewHolder.mSummaryView.setVisibility(View.GONE);
@@ -99,13 +97,12 @@ public class SummaryAdapter extends BaseAdapter {
             viewHolder.mSummaryView.setVisibility(View.VISIBLE);
             viewHolder.mSummaryView.setText(summary);
         }
-        resetPadding(i, viewHolder.mMainLayout);
-        int[] iArr = this.mTextColor;
-        if (iArr != null && i >= 0 && i < iArr.length) {
-            viewHolder.mItemView.setTextColor(iArr[i]);
+        resetPadding(position, viewHolder.mMainLayout);
+        if (mTextColor != null && position >= 0 && position < mTextColor.length) {
+            viewHolder.mItemView.setTextColor(mTextColor[position]);
         }
         if (viewHolder.mDivider != null) {
-            if (getCount() > 1 && i != getCount() - 1) {
+            if (getCount() > 1 && position != getCount() - 1) {
                 viewHolder.mDivider.setVisibility(View.VISIBLE);
             } else {
                 viewHolder.mDivider.setVisibility(View.GONE);
@@ -115,18 +112,16 @@ public class SummaryAdapter extends BaseAdapter {
         return view;
     }
 
-    @Override // android.widget.BaseAdapter, android.widget.Adapter
+    @Override
     public boolean hasStableIds() {
         return true;
     }
 
-    /* JADX DEBUG: Method merged with bridge method */
-    @Override // android.widget.Adapter
+    @Override
     public CharSequence getItem(int i) {
-        CharSequence[] charSequenceArr = this.mItems;
-        if (charSequenceArr == null) {
+        if (mItems == null) {
             return null;
         }
-        return charSequenceArr[i];
+        return mItems[i];
     }
 }
