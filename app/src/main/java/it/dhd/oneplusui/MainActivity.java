95d5c5a295d33e856bf617ef16dbff120fd881d0
DHD2280 <luigiconte98@gmail.com>
2024-11-10 19:45:47 +0100

    MainActivity: apply dynamic colors

--- app/src/main/java/it/dhd/oneplusui/MainActivity.java
+++ app/src/main/java/it/dhd/oneplusui/MainActivity.java
@@ -2,6 +2,8 @@ package it.dhd.oneplusui;
 
 import android.os.Bundle;
 
+import com.google.android.material.color.DynamicColors;
+
 import it.dhd.oneplusui.appcompat.app.OplusActivity;
 import it.dhd.oneplusui.sample.R;
 
@@ -10,6 +12,7 @@ public class MainActivity extends OplusActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DynamicColors.applyToActivityIfAvailable(this);
         setContentView(R.layout.activity_main);
 
         getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PrefFragment()).commit();
